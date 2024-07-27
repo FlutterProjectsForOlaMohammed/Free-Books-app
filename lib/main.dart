360e233bbe388a2ef8ddefb645eb0f9f6c721aab
Ola2024Mohammed <hawaiiict2022@gmail.com>
2024-07-28 02:29:41 +0300

    create Project Structure

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooksApp());
+}
+
+class BooksApp extends StatelessWidget {
+  const BooksApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
